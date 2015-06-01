ab647f15d1a5c8098707a4ab0755e6353a961268
Michael Osterhout <michael@revcontent.com>
2015-06-01 01:17:35 -0400

    copy scripts - this needs a better way

--- docs/gulpfile.js
+++ docs/gulpfile.js
@@ -5,6 +5,15 @@ var notify    = require("gulp-notify");
 var less      = require('gulp-less');
 var watch     = require('gulp-watch');
 
+var scripts = [
+  'bower_components/angular/angular.js',
+  'bower_components/angular-aria/angular-aria.js',
+  'bower_components/angular-animate/angular-animate.js',
+  'bower_components/angular-ui-router/release/angular-ui-router.js',
+  'bower_components/angular-material/angular-material.js',
+  'bower_components/angular-messages/angular-messages.js'
+]
+
 var svgs = [
   'bower_components/material-design-icons/navigation/svg/production/ic_arrow_forward_36px.svg',
   'bower_components/material-design-icons/navigation/svg/production/ic_arrow_back_36px.svg'
@@ -37,4 +46,9 @@ gulp.task('process-css', function () {
 gulp.task('copy-svg', function() {
     gulp.src(svgs)
       .pipe(gulp.dest('app/Resources/svg'));
+});
+
+gulp.task('copy-scripts', function() {
+    gulp.src(scripts)
+      .pipe(gulp.dest('app/Resources/js'));
 });
