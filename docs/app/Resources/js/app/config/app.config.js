33f62475e843529ac026570e490230c5d55291a7
Michael Osterhout <michael@revcontent.com>
2015-06-03 17:14:03 -0400

    github link

--- docs/app/Resources/js/app/config/app.config.js
+++ docs/app/Resources/js/app/config/app.config.js
@@ -1,5 +1,6 @@
 app.config(function($mdIconProvider) {
     $mdIconProvider
+        .icon('github', '/app/Resources/svg/github.svg')
         .icon('arrow_forward', '/app/Resources/svg/ic_arrow_forward_36px.svg')
         .icon('arrow_back', '/app/Resources/svg/ic_arrow_back_36px.svg');
 });
